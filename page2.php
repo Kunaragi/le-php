cd2ba44f6550c7bff24e172efdff3839b1adacb7
Radoslaw Sowa <radeksowa99@gmail.com>
2016-11-24 09:02:09 +0000

    Create page2.php

---
+++ page2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<h1>Question n°2</h1>
+  <h2>The elephant when "for" (again :) )</h2>
+    <?php
+      for ($i=100;$i>=1;$i--){
+        echo ($i);
+        echo ("-");
+      }
+    ?>
+
+<h2>The elephant when "while" (and again and again!) </h2>
+    <?php
+      $i=100;
+      while ($i>=1){
+        echo($i);
+        echo("-");
+        $i=$i-1;
+      }
+    ?>
+</html>
